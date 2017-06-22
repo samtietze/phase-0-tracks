7f3418b349f4e2f3615c61ef64c5fd854c24e0b9
Sam Tietze <samtietze@gmail.com>
2017-06-22 19:17:54 -0400

    Create form

---
+++ ruby/hamster.rb
@@ -0,0 +1,20 @@
+puts"Please enter your hamsters name:"
+hamstername = gets.chomp
+valid_input = false
+until valid_input
+  puts"Please enter the volume_level:"
+  hamstervol = gets.chomp
+  if hamstervol.to_i.between?(1,10)
+    valid_input = true
+  elsif puts "Please choose a number BETWEEN 1 and 10"
+  end
+end
+puts"What color is the hamster?"
+color =  gets.chomp
+puts"Would this hamster be a good candidate for adoption?"
+adoption = gets.chomp
+puts"What's this hamster's age"
+age = gets.chomp
+if age == ""
+  age = nil
+end
