614b5e3268f9788f3d934b3a786a0f8037fe82d9
Sam Tietze <samtietze@gmail.com>
2017-06-27 15:21:13 -0400

    pause for ordinal research

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,42 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
-# => “InVeStIgAtIoN”
+"iNvEsTiGaTiOn".swapcase
+=> “InVeStIgAtIoN”
 
-# "zom".<???>
-# => “zoom”
+"zom".insert(1, "o")
+=> “zoom”
 
-# "enhance".<???>
-# => "    enhance    "
+"enhance".rjust(8).ljust(12)
+=> "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
-# => "STOP! YOU’RE UNDER ARREST!"
+"Stop! You’re under arrest!".upcase
+=> "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
-#=> "the usual suspects"
+"the usual".concat(" suspects")
+=> "the usual suspects"
 
-# " suspects".<???>
-# => "the usual suspects"
+" suspects".prepend("the usual")
+=> "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
-# => "The case of the disappearing last lette"
+"The case of the disappearing last letter".chop
+=> "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
-# => "he mystery of the missing first letter"
+"The mystery of the missing first letter".delete("T")
+=> "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+"Elementary,    my   dear        Watson!".squeeze(" ")
+=> "Elementary, my dear Watson!"
+
+# Accidentally used an array method here...
+# "Elementary,    my   dear        Watson!".split(" ").join(" ")
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
-# => 122 
+"z".ord
+=> 122
 # (What is the significance of the number 122 in relation to the character z?)
+# I don't yet know how this information is used in Ruby, but this is a mathematical concept that indicates sequence.
+# Ruby might be using this as a more linguistic-oriented form in order to automatically attach suffixes to dates and other counters (1 -> 1st, etc.).
 
 # "How many times does the letter 'a' appear in this string?".<???>
 # => 4
