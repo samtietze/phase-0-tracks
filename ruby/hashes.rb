65739183d486a12f86c8789246ed8ca3b84fd5bc
Sam Tietze <samtietze@gmail.com>
2017-06-28 12:41:43 -0400

    Pseudocode form details

---
+++ ruby/hashes.rb
@@ -0,0 +1,20 @@
+# Creating a form using Ruby that allows
+# users to store both keys and their values in
+# a hash.
+  # This could be looped, but the instructions
+  # indicate it's unnecessary.
+  # User needs to input client name, age, children
+  # decor theme, etc.
+    # Since no loop, fill out the hash's
+    # keys ahead of time; like a proper form.
+# The keys should all be entered as symbols.
+# Once the form has been completed, all
+# of its values should be printed back to the user.
+
+# Prompt the user for any updates once the form
+# has been completed. If the user enters "yes" or
+# other affirmative:
+  # Transfer the user's input to a symbol (assume
+  # no mistakes will be made), and then let them
+  # enter a new value for that key.
+  # Print the final version!
