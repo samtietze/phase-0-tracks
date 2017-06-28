f32dcbf1d443a9681d17e026e1fbebbff51d054a
Sam Tietze <samtietze@gmail.com>
2017-06-28 13:03:29 -0400

    Fill in general outline of form

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -18,3 +18,38 @@
   # no mistakes will be made), and then let them
   # enter a new value for that key.
   # Print the final version!
+
+puts "Please enter your client's name:"
+client_name = gets.chomp
+puts "Please enter your client's age:"
+client_age = gets.chomp.to_i
+puts "Please enter the number of children in this household:"
+child_count = gets.chomp.to_i
+puts "Please provide a decor style the client wishes to use"
+style = gets.chomp
+puts "Does the client need drapes?"
+client_windows = gets.chomp
+puts "Are the client's floors carpeted?"
+client_floors = gets.chomp
+
+if client_windows.include?("no")
+  client_windows = false
+else
+  client_windows = true
+end
+if client_floors.include?("no")
+  client_floors = false
+else
+  client_floors = true
+end
+
+design_form = {
+  name: "#{client_name}",
+  age: client_age,
+  children: child_count,
+  decor_theme: "#{style}",
+  drapes: (nil if client_windows == false),
+  carpet: (nil if client_floors == false),
+}
+design_form.delete_if {|key, value| value.nil?}
+p design_form
