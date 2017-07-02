363ac0fd25af1e37ef66e50d1c12079076641585
Sam Tietze <samtietze@gmail.com>
2017-07-02 16:52:12 -0400

    Write method to simplify code

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -12,11 +12,28 @@
 # altering those characters.
 
 puts "Please enter your first and last name:"
-real_name = gets.chomp.downcase!
+real_name = gets.chomp.downcase
+# real_name = "Zellie Heinz Uruguay".downcase!
 
 def alias_manager(name)
   swap = name.split(' ').reverse.join(' ')
   swap_chars = swap.chars
+  # After getting this code working, I've gone back
+  # and written a short method that should work for
+  # both vowels and consonants. The same pseudo
+  # that I wrote out previously should apply here.
+  # We need to declare a number var ("x") in order
+  # to match it with the vowel/consonant index, but
+  # also get the NEXT position in that array.
+  def letter_check (ary, alpha, count)
+    x = alpha.index(ary[count]) + 1
+    if alpha[x].nil? # edge cases such as "u" or "z"
+      return alpha[0]
+    else
+      return alpha[x] # this index item will be shoveled
+      # into the spy name array.
+    end
+  end
   # Now we need a couple of letter-specific arrays
   # in order to run comparison operators between
   # each element at each index point.
@@ -26,37 +43,21 @@ def alias_manager(name)
   spy_name = [] # Shoveling into this array.
   swap_chars.each do
     # We must determine if the character is in the
-    # vowel or the consonant array.
-    # Then we need to find its index and move it
-    # to the next one.
-    # The edge cases in this operation are quite
-    # annoying. Since each array has a final
-    # element, each array has to be equipped
-    # with a control flow statement:
+    # vowel or the consonant array. It doesn't seem to
+    # be any more or less important to have these
+    # control flow statements inside the method or
+    # used later on in the loop.
     if vowels.include?(swap_chars[i])
-      x = vowels.index(swap_chars[i]) + 1
-      if vowels[x].nil? # i.e. "u"
-        spy_name << vowels[0]
-      else
-        spy_name << vowels[x]
-      end
-      i += 1
+      spy_name << letter_check(swap_chars, vowels, i)
     elsif consonants.include?(swap_chars[i])
-      x = consonants.index(swap_chars[i]) + 1
-      if consonants[x].nil? # i.e. "z"
-        spy_name << consonants[0]
-      else
-        spy_name << consonants[x]
-      end
-      i += 1
-    # Could not get this section work appropriately:
+      spy_name << letter_check(swap_chars, consonants, i)
+    # Could not get this section work appropriately with capitalization:
     # elsif swap_chars[i] == "-"
     #   spy_name << "-"
-    #   i += 1
     else
       spy_name << " "
-      i += 1
     end
+    i += 1
   end
 # remember to capitalize result
 
@@ -73,7 +74,7 @@ def alias_manager(name)
   # In this case it'll capitalize the first and
   # last names. However, I don't know how to handle
   # the edge case of an Arabic suffix that remains
-  # lower-case ("al-" or "ibn" or "bin").
+  # lower-case ("al-," "ibn," or "bin").
 
   final = spy_name.join.split.map(&:capitalize)*(' ')
 end
