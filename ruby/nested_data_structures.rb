f1a53a297285985c57818f4aac9f0b9974746a2e
Sam Tietze <samtietze@gmail.com>
2017-07-06 12:55:39 -0500

    Create coffee shop data structure

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,44 @@
+# Nested data
+# I'm unoriginal, so let's build a coffee shop.
+
+coffee_shop = {
+  first_location: {
+    address: '123 Fake Street'
+    seating: {
+      fire_code_restriction: 20
+      lounge_area: false
+    }
+    for_here_mugs: false
+    coffee_in_stock: [
+      "Light Roast", "Dark Roast", "Espresso"
+    ]
+    }
+    cafe: true
+
+  }
+  second_location: {
+    address: '50000 N Washington Ave'
+    seating: {
+      fire_code_restriction: 100
+      lounge_area: false
+    }
+    for_here_mugs: true
+    coffee_in_stock: [
+      "Light Roast", "Espresso"
+    ]
+    cafe: false
+  }
+  third_location: {
+    address: '8080 Real Pkwy'
+    seating: {
+      fire_code_restriction: 150
+      lounge_area: true
+    }
+    for_here_mugs: true
+    coffee_in_stock: [
+      "Light Roast", "Espresso"
+    ]
+    cafe: true
+  }
+}
+
