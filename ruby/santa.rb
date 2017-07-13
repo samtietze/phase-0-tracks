84056296e77da4502bd1eb518f44a1efaf0d4b67
Sam Tietze <samtietze@gmail.com>
2017-07-13 16:41:50 -0400

    Finish release 1 iteration

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -18,6 +18,19 @@ class Santa
 
 end
 
-santa = Santa.new("agender", "black")
-santa.speak
-santa.eat_milk_and_cookies("snickerdoodle")
+# santa = Santa.new("agender", "black")
+# santa.speak
+# santa.eat_milk_and_cookies("snickerdoodle")
+
+santas = []
+genders = ["male", "female", "agender", "bigender", "gender is a social construct", "N/A"]
+ethnicities = ["caucasion", "hispanic", "black", "Chinese", "Japanese", "Indian"]
+
+genders.length.times do
+  puts "Here comes Santa"
+  santas << Santa.new(genders.sample, ethnicities.sample)
+  santas.sample.speak
+end
+p santas
+
+
