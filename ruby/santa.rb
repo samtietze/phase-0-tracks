7f6fbd016ff0534402d1e460654906b4742a6005
Sam Tietze <samtietze@gmail.com>
2017-07-13 18:34:14 -0400

    Finally move attributes where they need to be. Unclear! Resolved! Create two methods for setting attributes

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -11,10 +11,18 @@ class Santa
     puts "Initializing Santa instance..."
     @gender = gender
     @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
   end
 
-  reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-  age = 0
+  def celebrate_birthday
+    @age += 1
+  end
+
+  def get_mad_at(reindeer)
+    @reindeer_ranking.delete_if {|name| name == "#{reindeer}"}
+    @reindeer_ranking.insert(-1, "#{reindeer}")
+  end
 
 end
 
@@ -22,15 +30,15 @@ end
 # santa.speak
 # santa.eat_milk_and_cookies("snickerdoodle")
 
-santas = []
-genders = ["male", "female", "agender", "bigender", "gender is a social construct", "N/A"]
-ethnicities = ["caucasion", "hispanic", "black", "Chinese", "Japanese", "Indian"]
+# santas = []
+# genders = ["male", "female", "agender", "bigender", "gender is a social construct", "N/A"]
+# ethnicities = ["caucasion", "hispanic", "black", "Chinese", "Japanese", "Indian"]
 
-genders.length.times do
-  puts "Here comes Santa"
-  santas << Santa.new(genders.sample, ethnicities.sample)
-  santas.sample.speak
-end
-p santas
+# genders.length.times do
+#   puts "Here comes Santa"
+#   santas << Santa.new(genders.sample, ethnicities.sample)
+#   santas.sample.speak
+# end
+# p santas
 
 
