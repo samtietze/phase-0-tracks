c7938d95a9f3e9375870e922ff0a5dfd409bdcc2
Sam Tietze <samtietze@gmail.com>
2017-07-15 16:17:07 -0400

    Pseudocode class out a little bit and add starter code

---
+++ ruby/gameclass/word_game.rb
@@ -0,0 +1,27 @@
+# Write a class that is capable of allowing
+# its various attributes to be accessed from outside
+# the class. One user will input a word to the
+# class, and another user will input guesses.
+
+# Since the guesses will "fill in" the blanks if
+# they're correct, the second user will need access
+# to the instance variable that displays the blanks.
+
+# The second user's amount of guesses are related
+# to the length of the word. This doesn't mean it
+# needs to equal the length.
+  # If the user guesses the same letter again,
+  # this shouldn't count against the user.
+
+# Each time the user guesses, the game should print
+# out its current status. That is, "_______"
+# should become "___e___", etc.
+
+# Once the user has guessed the word successfully
+# they should be given a congratulatory message.
+# If the user loses, they should get a taunting
+# message instead.
+
+class WordGame
+
+end
---
+++ ruby/gameclass/word_game_spec.rb
@@ -0,0 +1,5 @@
+require_relative 'word_game'
+
+describe WordGame do
+
+end
