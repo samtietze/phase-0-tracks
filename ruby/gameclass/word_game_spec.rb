6ac7770d2e7bb2d5ae47c85799cd4ff8305cd80b
Sam Tietze <samtietze@gmail.com>
2017-07-15 18:30:51 -0400

    Outline most of the necessary tests to get started on this beast

--- ruby/gameclass/word_game_spec.rb
+++ ruby/gameclass/word_game_spec.rb
@@ -1,5 +1,51 @@
 require_relative 'word_game'
 
 describe WordGame do
+  let(:hangman) { WordGame.new("football")}
+
+  it "stores the challenge word given by the first user" do
+    expect(hangman.reveal_answer).to eq "football"
+  end
+
+  it "revises the number of guesses based on the challenge word length" do
+    expect(hangman.total_guesses).to eq 13
+  end
+
+  it "replaces the challenge word with blanks" do
+    expect(hangman.display).to eq "________"
+  end
+
+  it "reveals a correctly guessed letter" do
+    hangman.user_guess("o")
+    expect(hangman.display).to eq "_oo_____"
+  end
+
+  it "mocks the user for guessing incorrectly" do
+    hangman.user_guess("z")
+    expect(hangman.nope).to eq "You must be daft"
+  end
+
+  it "counts guesses down" do
+    hangman.user_guess("z")
+    expect(hangman.guesses_left).to eq 12
+  end
+
+  it "doesn't count down for repeated guesses" do
+    hangman.user_guess("z")
+    hangman.guesses_left = 12
+    hangman.user_guess("z")
+    expect(hangman.guesses_left).to eq 12
+  end
+
+  it "ends the game if the number of guesses reaches 0" do
+    hangman.guesses_left = 0
+    expect(hangman.game_over).to be true
+  end
+
+  it "displays a congratulatory message if the user guesses the word" do
+    hangman.display == hangman.reveal_answer
+    expect(hangman.you_win).to eq "Congratulations!"
+  end
+
 
 end
