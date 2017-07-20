ebd9f9d0795818903a9091a955f924611a2f87d8
Sam Tietze <samtietze@gmail.com>
2017-07-20 16:37:11 -0400

    Create reverse method

---
+++ js/explore.js
@@ -0,0 +1,17 @@
+// Write a method that takes in a string and outputs its reverse.
+// input: a string
+// steps: could loop through this string and revise characters that way.
+// Could also create a blank string and feed characters into it.
+// For each character in the input string, we
+// need to put its last characters into the new
+// string.
+// output: reverse that string
+var str = "hello";
+function reverse(string) {
+  var reverseString = ""
+  for (i = str.length - 1; i >= 0; i--) {
+    reverseString += str.charAt(i);
+    console.log(reverseString);
+  }
+}
+reverse(str);
