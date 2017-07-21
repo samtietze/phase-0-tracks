e3caa4ffb87262262880a30334263e29649eb589
Sam Tietze <samtietze@gmail.com>
2017-07-21 14:44:26 -0500

    Pseudocode a potential loop/conditional setup

---
+++ js/algos.js
@@ -0,0 +1,15 @@
+// Release 0
+// Write a function that takes an array of strings
+// and returns the longest string from that array.
+// Hint: .length method works in javascript.
+
+// Function input: array of strings
+// Function output: the longest string within the
+// array.
+// Possible solutions: LOOP through the array, and
+// use a conditional.
+// IF the first string is smaller than the next string
+// move on to the next index.
+// ELSE IF the first string is bigger than the next,
+// compare the first string to the third item in the
+// index and repeat with the modified IF statement.
