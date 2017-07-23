0356012c2b9a6547e8cb1726ff681e433efaabf8
Sam Tietze <samtietze@gmail.com>
2017-07-23 16:58:00 -0500

    Create object of names and colors

--- js/data_structures.js
+++ js/data_structures.js
@@ -14,3 +14,22 @@ function addName(name, array) {
 }
 
 console.log(addName("Lenny", names));
+
+// Commenting this out because it does not work. Overwrites the
+// keys in the object rather than adding to the object.
+
+// names.forEach(function(element, index) {
+//   horseObject = {};
+//   horseObject[element] = colors[index];
+//   return horseObject;
+// });
+
+// console.log(horseObject);
+
+addColor("super sparkle", colors);
+addName("Harry Caray", names);
+horseObject = {}
+for(i = 0; i <names.length; i++) {
+  horseObject[names[i]] = colors[i];
+}
+console.log(horseObject);
