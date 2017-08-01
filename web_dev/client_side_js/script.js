bce5b97edfb4cd1d3c559def76671b2b0d4e040c
Sam Tietze <samtietze@gmail.com>
2017-08-01 14:45:55 -0400

    Create simple script for hiding/unhiding an image

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,1 +1,10 @@
 console.log("The script is running!");
+
+function hideImage() {
+  var img = document.getElementById("bookimg");
+  if (img.style.visibility === "hidden") {
+    img.style.visibility = "visible";
+  } else {
+    img.style.visibility = "hidden";
+  }
+}
